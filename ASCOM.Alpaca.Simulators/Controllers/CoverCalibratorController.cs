ff996d52b7a7a622ab1b8249bcb42de6d05fdc08
Daniel Van Noord <sg1mash98@gmail.com>
2021-01-04 20:41:59 -0500

    Added CovCal Controller

---
+++ ASCOM.Alpaca.Simulators/Controllers/CoverCalibratorController.cs
@@ -0,0 +1,401 @@
+﻿using ASCOM.Alpaca.Responses;
+using ASCOM.Standard.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASCOM.Alpaca.Simulators
+{
+    [ServiceFilter(typeof(AuthorizationFilter))]
+    [ApiController]
+    public class CoverCalibrator : Controller
+    {
+        private const string APIRoot = "api/v1/covercalibrator/";
+
+        #region Common Methods
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/Action")]
+        public StringResponse Action(int DeviceNumber, [FromForm] string Action, [FromForm] string Parameters, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, $"Action: {Action}, Parameters {Parameters}");
+
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).Action(Action, Parameters));
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CommandBlind")]
+        public Response CommandBlind(int DeviceNumber, [FromForm] string Command, [FromForm] bool Raw = false, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, $"Command {Command}, Raw {Raw}");
+
+                DeviceManager.GetCoverCalibrator(DeviceNumber).CommandBlind(Command, Raw);
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CommandBool")]
+        public BoolResponse CommandBool(int DeviceNumber, [FromForm] string Command, [FromForm] bool Raw = false, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, $"Command {Command}, Raw {Raw}");
+                return new BoolResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).CommandBool(Command, Raw));
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<BoolResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CommandString")]
+        public StringResponse CommandString(int DeviceNumber, [FromForm] string Command, [FromForm] bool Raw = false, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, $"Command {Command}, Raw {Raw}");
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).CommandString(Command, Raw));
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/Connected")]
+        public BoolResponse Connected(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new BoolResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).Connected);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<BoolResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/Connected")]
+        public Response Connected(int DeviceNumber, [FromForm] bool Connected, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, $"Connected {Connected}");
+
+                if (Connected || !ServerSettings.PreventRemoteDisconnects)
+                {
+                    DeviceManager.GetCoverCalibrator(DeviceNumber).Connected = Connected;
+                }
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/Description")]
+        public StringResponse Description(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).Description);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/DriverInfo")]
+        public StringResponse DriverInfo(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).DriverInfo);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/DriverVersion")]
+        public StringResponse DriverVersion(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).DriverVersion);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/InterfaceVersion")]
+        public IntResponse InterfaceVersion(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new IntResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).InterfaceVersion);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<IntResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/Name")]
+        public StringResponse Name(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new StringResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).Name);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/SupportedActions")]
+        public StringListResponse SupportedActions(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new StringListResponse(ClientTransactionID, TransactionID, new List<string>(DeviceManager.GetCoverCalibrator(DeviceNumber).SupportedActions.Cast<string>().ToList()));
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<StringListResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        #region IDisposable Members
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/Dispose")]
+        public Response Dispose(int DeviceNumber, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                if (!ServerSettings.PreventRemoteDisposes)
+                {
+                    DeviceManager.GetCoverCalibrator(DeviceNumber).Dispose();
+                }
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        #endregion IDisposable Members
+
+        #endregion Common Methods
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/CoverState")]
+        public CoverStatusResponse CoverState(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new CoverStatusResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).CoverState);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<CoverStatusResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/CalibratorState")]
+        public CalibratorStatusResponse CalibratorState(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new CalibratorStatusResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorState);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<CalibratorStatusResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/Brightness")]
+        public IntResponse Brightness(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new IntResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).Brightness);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<IntResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpGet]
+        [Route(APIRoot + "{DeviceNumber}/MaxBrightness")]
+        public IntResponse MaxBrightness(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                return new IntResponse(ClientTransactionID, TransactionID, DeviceManager.GetCoverCalibrator(DeviceNumber).MaxBrightness);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<IntResponse>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/OpenCover")]
+        public Response OpenCover(int DeviceNumber, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                DeviceManager.GetCoverCalibrator(DeviceNumber).OpenCover();
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CloseCover")]
+        public Response CloseCover(int DeviceNumber, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                DeviceManager.GetCoverCalibrator(DeviceNumber).CloseCover();
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/HaltCover")]
+        public Response HaltCover(int DeviceNumber, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                DeviceManager.GetCoverCalibrator(DeviceNumber).HaltCover();
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CalibratorOn")]
+        public Response CalibratorOn(int DeviceNumber, [FromForm] int Brightness, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID, Brightness.ToString());
+
+                DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorOn(Brightness);
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+
+        [HttpPut]
+        [Route(APIRoot + "{DeviceNumber}/CalibratorOff")]
+        public Response CalibratorOff(int DeviceNumber, [FromForm] int ClientID = -1, [FromForm] uint ClientTransactionID = 0)
+        {
+            var TransactionID = ServerManager.ServerTransactionID;
+            try
+            {
+                Logging.LogAPICall(HttpContext.Connection.RemoteIpAddress, HttpContext.Request.Path.ToString(), ClientID, ClientTransactionID, TransactionID);
+                DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorOff();
+
+                return new Response() { ClientTransactionID = ClientTransactionID, ServerTransactionID = TransactionID };
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelpers.ExceptionResponseBuilder<Response>(ex, ClientTransactionID, TransactionID);
+            }
+        }
+    }
+}
