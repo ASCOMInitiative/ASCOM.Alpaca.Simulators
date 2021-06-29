3cc0606ec1761f581635c141ec32762444468fde
Daniel Van Noord <sg1mash98@gmail.com>
2021-06-28 20:44:11 -0400

    Added catch all API to return Bad Request on nonexistent API routes

---
+++ ASCOM.Alpaca.Simulators/Controllers/CatchAll.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ASCOM.Alpaca.Simulators
+{
+    /// <summary>
+    /// This controller catches all requests on the /api that do not have a controller and return an HTTP 400.
+    /// To not have this catch something create the route.
+    /// </summary>
+    [ServiceFilter(typeof(AuthorizationFilter))]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class CatchAll : Controller
+    {
+        [HttpGet]
+        [Route("api", Order = 999)]
+        public async Task<IActionResult> CatchAllApiGets()
+        {
+            return BadRequest("Alpaca API route was not found on this driver");
+        }
+
+        [HttpPut]
+        [Route("api", Order = 999)]
+        public async Task<IActionResult> CatchAllApiPuts()
+        {
+            return BadRequest("Alpaca API route was not found on this driver");
+        }
+    }
+}
