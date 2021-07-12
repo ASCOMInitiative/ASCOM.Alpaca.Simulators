fcd7e58409dff70a93a5a9dd86df0b2e197e330c
Daniel Van Noord <sg1mash98@gmail.com>
2021-07-12 14:18:59 -0400

    Cleaned up route capitalization for CovCal APIs

--- ASCOM.Alpaca.Simulators/Controllers/CoverCalibratorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/CoverCalibratorController.cs
@@ -19,63 +19,63 @@ namespace ASCOM.Alpaca.Simulators
         }
 
         [HttpGet]
-        [Route("{DeviceNumber}/CoverState")]
+        [Route("{DeviceNumber}/coverstate")]
         public ActionResult<IntResponse> CoverState([DefaultValue(0)] int DeviceNumber, uint ClientID = 0, uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => (int)DeviceManager.GetCoverCalibrator(DeviceNumber).CoverState, DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpGet]
-        [Route("{DeviceNumber}/CalibratorState")]
+        [Route("{DeviceNumber}/calibratorstate")]
         public ActionResult<IntResponse> CalibratorState([DefaultValue(0)] int DeviceNumber, uint ClientID = 0, uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => (int)DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorState, DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpGet]
-        [Route("{DeviceNumber}/Brightness")]
+        [Route("{DeviceNumber}/brightness")]
         public ActionResult<IntResponse> Brightness([DefaultValue(0)] int DeviceNumber, uint ClientID = 0, uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).Brightness, DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpGet]
-        [Route("{DeviceNumber}/MaxBrightness")]
+        [Route("{DeviceNumber}/maxbrightness")]
         public ActionResult<IntResponse> MaxBrightness([DefaultValue(0)] int DeviceNumber, uint ClientID = 0, uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).MaxBrightness, DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpPut]
-        [Route("{DeviceNumber}/OpenCover")]
+        [Route("{DeviceNumber}/opencover")]
         public ActionResult<Response> OpenCover([DefaultValue(0)] int DeviceNumber, [FromForm] uint ClientID = 0, [FromForm] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).OpenCover(), DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpPut]
-        [Route("{DeviceNumber}/CloseCover")]
+        [Route("{DeviceNumber}/closecover")]
         public ActionResult<Response> CloseCover([DefaultValue(0)] int DeviceNumber, [FromForm] uint ClientID = 0, [FromForm] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).CloseCover(), DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpPut]
-        [Route("{DeviceNumber}/HaltCover")]
+        [Route("{DeviceNumber}/haltcover")]
         public ActionResult<Response> HaltCover([DefaultValue(0)] int DeviceNumber, [FromForm] uint ClientID = 0, [FromForm] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).HaltCover(), DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
 
         [HttpPut]
-        [Route("{DeviceNumber}/CalibratorOn")]
+        [Route("{DeviceNumber}/calibratoron")]
         public ActionResult<Response> CalibratorOn([DefaultValue(0)] int DeviceNumber, [DefaultValue(0)][Required][FromForm] int Brightness, [FromForm] uint ClientID = 0, [FromForm] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorOn(Brightness), DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Brightness={Brightness}");
         }
 
         [HttpPut]
-        [Route("{DeviceNumber}/CalibratorOff")]
+        [Route("{DeviceNumber}/calibratoroff")]
         public ActionResult<Response> CalibratorOff([DefaultValue(0)] int DeviceNumber, [FromForm] uint ClientID = 0, [FromForm] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetCoverCalibrator(DeviceNumber).CalibratorOff(), DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
