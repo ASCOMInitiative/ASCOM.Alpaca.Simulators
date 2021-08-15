c3b54d7307e4a79bcab6fbae9251606e13f4b914
Daniel Van Noord <sg1mash98@gmail.com>
2021-08-14 22:12:35 -0400

    Added a notice to the swagger document

--- ASCOM.Alpaca.Simulators/Startup.cs
+++ ASCOM.Alpaca.Simulators/Startup.cs
@@ -48,7 +48,7 @@ namespace ASCOM.Alpaca.Simulators
             {
                 services.AddSwaggerGen(c =>
                 {
-                    c.SwaggerDoc("v1", new OpenApiInfo { Title = $"{ServerSettings.ServerName}", Version = "v1" });
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = $"{ServerSettings.ServerName}", Description = "Please note that the Alpaca API documentation on the ASCOM website is the canonical version. There are several issues with this auto generated version that will be resolved in future versions. This is currently provided only for testing the simulators.", Version = "v0" });
                 });
             }
 
