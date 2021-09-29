4223fd2e632375a11eab3cb396819a9c664c3907
Daniel Van Noord <sg1mash98@gmail.com>
2021-09-28 20:19:53 -0400

    Switched to lower case for packages

--- ASCOM.Alpaca.Simulators/publish-all.bat
+++ ASCOM.Alpaca.Simulators/publish-all.bat
@@ -1,11 +1,11 @@
-dotnet publish -c Release -r linux-arm --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.linux-armhf
-dotnet publish -c Release -r linux-arm64 --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.linux-aarch64
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.linux-x64
+dotnet publish -c Release -r linux-arm --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.linux-armhf
+dotnet publish -c Release -r linux-arm64 --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.linux-aarch64
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.linux-x64
 
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.windows-x64
-dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.windows-x86
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.windows-x64
+dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.windows-x86
 
-dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ASCOM.Alpaca.Simulators.macos-x64
+dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true -o ./bin/ascom.alpaca.simulators.macos-x64
 
 
 echo "Note, these builds are not Ready to Run so they will run slower"
