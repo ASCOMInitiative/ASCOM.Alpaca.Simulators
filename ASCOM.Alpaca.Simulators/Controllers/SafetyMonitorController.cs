0a8752ac9a1b3be2250c933a4257e8ef62a729e6
Daniel Van Noord <sg1mash98@gmail.com>
2021-11-03 20:10:39 -0400

    Annotated SafetyMonitor controller

--- ASCOM.Alpaca.Simulators/Controllers/SafetyMonitorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SafetyMonitorController.cs
@@ -21,12 +21,24 @@ namespace ASCOM.Alpaca.Simulators
             return DeviceManager.GetSafetyMonitor(DeviceNumber);
         }
 
+        /// <summary>
+        /// Indicates whether the monitored state is safe for use.
+        /// </summary>
+        /// <remarks>
+        /// <para>Indicates whether the monitored state is safe for use. True if the state is safe, False if it is unsafe.</para>
+        /// </remarks>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
         [HttpGet]
         [Route("{DeviceNumber}/issafe")]
-        public ActionResult<BoolResponse> IsSafe([DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription)] int DeviceNumber,
- [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)]uint ClientID = 0, 
- [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0
-)
+        public ActionResult<BoolResponse> IsSafe(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription)] int DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)]uint ClientID = 0, 
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
         {
             return ProcessRequest(() => DeviceManager.GetSafetyMonitor(DeviceNumber).IsSafe, DeviceManager.ServerTransactionID, ClientID, ClientTransactionID);
         }
