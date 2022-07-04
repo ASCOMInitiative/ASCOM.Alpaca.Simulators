3a785b4ab3bbc2d10a20273187b3938300b71680
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-03 22:05:27 -0400

    Began adding simulator control api

---
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -0,0 +1,40 @@
+﻿using ASCOM.Common.Alpaca;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+
+namespace ASCOM.Alpaca.Simulators
+{
+    [ServiceFilter(typeof(AuthorizationFilter))]
+    [ApiController]
+    [Route("simulator/v1/")]
+    public class SimulatorController
+    {
+        public const string APIRoot = "simulator/v1/";
+
+        /// <summary>
+        /// Indicates whether the monitored state is safe for use.
+        /// </summary>
+        /// <remarks>
+        /// <para>Indicates whether the monitored state is safe for use. True if the state is safe, False if it is unsafe.</para>
+        /// </remarks>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("camera/{DeviceNumber}/reset")]
+        public ActionResult<BoolResponse> ResetCamera(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+            return null;
+        }
+    }
+}
