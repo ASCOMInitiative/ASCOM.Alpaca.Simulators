62f90d3add3d200c91bfb889376bb889e98aa3b6
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-11 21:53:36 -0400

    Added Observing Conditions reset API

--- ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -37,10 +37,15 @@ namespace ASCOM.Alpaca.Simulators
         {
             return DeviceManager.GetFilterWheel((uint)InstanceID) as ASCOM.Simulators.FilterWheel;
         }
-
+        
         private ASCOM.Simulators.Focuser FocuserAccess(uint InstanceID)
         {
             return DeviceManager.GetFocuser((uint)InstanceID) as ASCOM.Simulators.Focuser;
+        }
+
+        private ASCOM.Simulators.ObservingConditions ObservingConditionsAccess(uint InstanceID)
+        {
+            return DeviceManager.GetObservingConditions((uint)InstanceID) as ASCOM.Simulators.ObservingConditions;
         }
 
 
@@ -169,6 +174,33 @@ namespace ASCOM.Alpaca.Simulators
             return ProcessRequest(() =>
             {
                 FocuserAccess(DeviceNumber).Reset();
+            },
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
+        }
+
+        /// <summary>
+        /// OmniSim only API - Resets a device settings to the simulator default ObservingConditions 
+        /// </summary>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("observingconditions/{DeviceNumber}/reset")]
+        public ActionResult<Response> ResetObservingConditions(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+
+
+            return ProcessRequest(() =>
+            {
+                ASCOM.Simulators.OCSimulator.ClearProfile();
+                ASCOM.Simulators.OCSimulator.Init();
             },
             DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
         }
