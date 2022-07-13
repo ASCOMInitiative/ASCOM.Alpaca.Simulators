7fd26fa831d24af5ca799c8031c3c4f3749b7d9e
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-12 21:42:13 -0400

    Added rotator reset API

--- ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -204,5 +204,31 @@ namespace ASCOM.Alpaca.Simulators
             },
             DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
         }
+
+        /// <summary>
+        /// OmniSim only API - Resets a device settings to the simulator default ObservingConditions 
+        /// </summary>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("rotator/{DeviceNumber}/reset")]
+        public ActionResult<Response> ResetRotator(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+
+
+            return ProcessRequest(() =>
+            {
+                ASCOM.Simulators.RotatorHardware.ResetProfile();
+            },
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
+        }
     }
 }
