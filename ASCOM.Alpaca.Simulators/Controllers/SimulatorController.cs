c99ed180b52d638a69f6dfb778a01d740e19f864
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-13 22:46:08 -0400

    Added Safety Monitor reset API

--- ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -48,6 +48,11 @@ namespace ASCOM.Alpaca.Simulators
             return DeviceManager.GetObservingConditions((uint)InstanceID) as ASCOM.Simulators.ObservingConditions;
         }
 
+        private ASCOM.Simulators.SafetyMonitor SafetyMonitorAccess(uint InstanceID)
+        {
+            return DeviceManager.GetSafetyMonitor((uint)InstanceID) as ASCOM.Simulators.SafetyMonitor;
+        }
+
 
         /// <summary>
         /// OmniSim only API - Resets a device settings to the simulator default
@@ -227,6 +232,32 @@ namespace ASCOM.Alpaca.Simulators
             return ProcessRequest(() =>
             {
                 ASCOM.Simulators.RotatorHardware.ResetProfile();
+            },
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
+        }
+
+        /// <summary>
+        /// OmniSim only API - Resets a device settings to the simulator default ObservingConditions 
+        /// </summary>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("safetymonitor/{DeviceNumber}/reset")]
+        public ActionResult<Response> ResetSafetyMonitor(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+
+
+            return ProcessRequest(() =>
+            {
+                SafetyMonitorAccess(DeviceNumber).ResetProfile();
             },
             DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
         }
