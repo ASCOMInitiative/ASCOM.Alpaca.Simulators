ee4947ade6a55e3d9b5328279ed08869a48ce3aa
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-14 22:27:41 -0400

    Added Switch reset API

--- ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -53,6 +53,10 @@ namespace ASCOM.Alpaca.Simulators
             return DeviceManager.GetSafetyMonitor((uint)InstanceID) as ASCOM.Simulators.SafetyMonitor;
         }
 
+        private ASCOM.Simulators.Switch SwitchAccess(uint InstanceID)
+        {
+            return DeviceManager.GetSwitch((uint)InstanceID) as ASCOM.Simulators.Switch;
+        }
 
         /// <summary>
         /// OmniSim only API - Resets a device settings to the simulator default
@@ -258,6 +262,33 @@ namespace ASCOM.Alpaca.Simulators
             return ProcessRequest(() =>
             {
                 SafetyMonitorAccess(DeviceNumber).ResetProfile();
+            },
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
+        }
+
+        /// <summary>
+        /// OmniSim only API - Resets a device settings to the simulator default ObservingConditions 
+        /// </summary>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("switch/{DeviceNumber}/reset")]
+        public ActionResult<Response> ResetSwitch(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+
+
+            return ProcessRequest(() =>
+            {
+                SwitchAccess(DeviceNumber).ResetProfile();
+                SwitchAccess(DeviceNumber).ReadProfile();
             },
             DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
         }
