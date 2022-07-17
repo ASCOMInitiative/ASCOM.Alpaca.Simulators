c80495e8191af1c5bb32df1676b5b174f1d04ba2
Daniel Van Noord <sg1mash98@gmail.com>
2022-07-17 11:49:09 -0400

    Added Telescope reset API

--- ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
+++ ASCOM.Alpaca.Simulators/Controllers/SimulatorController.cs
@@ -1,4 +1,4 @@
-﻿using ASCOM.Common.Alpaca;
+using ASCOM.Common.Alpaca;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
@@ -290,7 +290,32 @@ namespace ASCOM.Alpaca.Simulators
                 SwitchAccess(DeviceNumber).ResetProfile();
                 SwitchAccess(DeviceNumber).ReadProfile();
             },
-            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Focuser to default settings.");
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Switch to default settings.");
+        }
+
+        /// <summary>
+        /// OmniSim only API - Resets a device settings to the simulator default ObservingConditions 
+        /// </summary>
+        /// <param name="DeviceNumber">Zero based device number as set on the server (A uint32 with a range of 0 to 4294967295)</param>
+        /// <param name="ClientID">Client's unique ID.</param>
+        /// <param name="ClientTransactionID">Client's transaction ID.</param>
+        /// <response code="200">Transaction complete or exception</response>
+        /// <response code="400" examples="Error message describing why the command cannot be processed">Method or parameter value error, check error message</response>
+        /// <response code="500" examples="Error message describing why the command cannot be processed">Server internal error, check error message</response>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Route("telescope/{DeviceNumber}/reset")]
+        public ActionResult<Response> ResetTelescope(
+            [DefaultValue(0)][SwaggerSchema(Strings.DeviceIDDescription, Format = "uint32")][Range(0, 4294967295)] uint DeviceNumber,
+            [SwaggerSchema(Description = Strings.ClientIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientID = 0,
+            [SwaggerSchema(Strings.ClientTransactionIDDescription, Format = "uint32")][Range(0, 4294967295)] uint ClientTransactionID = 0)
+        {
+            return ProcessRequest(() =>
+            {
+                ASCOM.Simulators.TelescopeHardware.ClearProfile();
+                ASCOM.Simulators.TelescopeHardware.Init();
+            },
+            DeviceManager.ServerTransactionID, ClientID, ClientTransactionID, $"Reseting Telescope to default settings.");
         }
     }
 }
