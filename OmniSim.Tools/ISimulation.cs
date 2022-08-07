4840633222ffc4f836a5967cc70080b5ea975fbc
Daniel Van Noord <sg1mash98@gmail.com>
2022-08-07 14:19:33 -0400

    Added ISimulation

---
+++ OmniSim.Tools/ISimulation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Simulators
+{
+    public interface ISimulation
+    {
+        void ResetSettings();
+    }
+}
