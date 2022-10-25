bf7e15c460d72e818ab3737ed3b8aa18571c5922
Daniel Van Noord <sg1mash98@gmail.com>
2022-10-25 11:20:31 -0400

    Use the port to detect if driver is already running

--- ASCOM.Alpaca.Simulators/Program.cs
+++ ASCOM.Alpaca.Simulators/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 
 namespace ASCOM.Alpaca.Simulators
 {
@@ -40,14 +41,12 @@ namespace ASCOM.Alpaca.Simulators
                 Console.WriteLine($"http://localhost:{ServerSettings.ServerPort}");
             }
 
-            //Already running, start the browser
-            //This was working fine for .Net Core 3.1. Initial tests for .Net 5 show a change in how single file deployments work on Linux
-            //This should probably be changed to a Mutex or another similar lock
+            //Already running, start the browser, detects based on port in use
             try
             {
-                if (System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1)
+                if(IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections().Any(con => con.LocalEndPoint.Port == ServerSettings.ServerPort))
                 {
-                    WriteAndLog("Detected driver already running, starting web browser on IP and Port");
+                    WriteAndLog("Detected driver port already open, starting web browser on IP and Port. If this fails something else is using the port");
                     StartBrowser(ServerSettings.ServerPort);
                     return;
                 }
