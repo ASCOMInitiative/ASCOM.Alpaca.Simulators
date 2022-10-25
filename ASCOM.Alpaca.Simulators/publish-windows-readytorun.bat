551a8b322726260e4394c289d80dd5d5a7470e7e
Daniel Van Noord <sg1mash98@gmail.com>
2022-10-25 11:09:34 -0400

    Build Windows ready to run as single file

--- ASCOM.Alpaca.Simulators/publish-windows-readytorun.bat
+++ ASCOM.Alpaca.Simulators/publish-windows-readytorun.bat
@@ -1,2 +1,2 @@
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/ascom.alpaca.simulators.windows-x64
-dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -o ./bin/ascom.alpaca.simulators.windows-x86
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -p:PublishSingleFile=true -o ./bin/ascom.alpaca.simulators.windows-x64
+dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=true /p:PublishReadyToRun=true /p:PublishReadyToRunShowWarnings=true -p:PublishSingleFile=true -o ./bin/ascom.alpaca.simulators.windows-x86
