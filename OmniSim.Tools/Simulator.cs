137154f66fa664ff92dff3493a834de9d0eb95c4
Daniel Van Noord <sg1mash98@gmail.com>
2022-11-30 20:18:48 -0500

    Began adding Simulator Class

---
+++ OmniSim.Tools/Simulator.cs
@@ -0,0 +1,26 @@
+﻿using ASCOM.Simulators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniSim.Tools
+{
+    public abstract class Simulator : ISimulation, IAlpacaDevice
+    {
+        public string DeviceName => throw new NotImplementedException();
+
+        public int DeviceNumber => throw new NotImplementedException();
+
+        public string UniqueID => throw new NotImplementedException();
+
+        public string GetXMLProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
