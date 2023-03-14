d437e897ab26cbbef9bf50a3a2fa0f835cf51d11
Daniel Van Noord <sg1mash98@gmail.com>
2023-03-13 20:35:37 -0400

    Remove DriverException from the Alpaca Response OpenAPI docs

---
+++ ASCOM.Alpaca.Simulators/SwaggerExcludeFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace ASCOM.Alpaca.Simulators
+{
+    public class SwaggerExcludeFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext schemaRegistry)
+        {
+            if (schema?.Properties == null || schemaRegistry.Type == null)
+                return;
+
+            // DriverException is part of a response but not part of the Alpaca API
+            var excludedProperties = schemaRegistry.Type.GetProperties()
+                                         .Where(t => t.Name == "DriverException");
+
+            foreach (var excludedProperty in excludedProperties)
+            {
+                if (schema.Properties.ContainsKey(excludedProperty.Name))
+                    schema.Properties.Remove(excludedProperty.Name);
+            }
+        }
+    }
+}
