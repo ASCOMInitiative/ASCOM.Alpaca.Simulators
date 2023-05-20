7f4f3cbb2096cf1f0e8f714951f47077206166e5
Daniel Van Noord <sg1mash98@gmail.com>
2023-05-20 14:39:42 -0400

    Began adding startup helpers

--- ASCOM.Alpaca.Razor/Discovery/DiscoveryManager.cs
+++ ASCOM.Alpaca.Razor/Discovery/DiscoveryManager.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
-namespace ASCOM.Alpaca.Simulators.Discovery
+namespace ASCOM.Alpaca
 {
     public static class DiscoveryManager
     {
