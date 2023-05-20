db364355619a4e45b8bd5d5604a20131d94f8730
Daniel Van Noord <sg1mash98@gmail.com>
2023-05-20 15:26:18 -0400

    Rebuilt registering discovery

--- ASCOM.Alpaca.Simulators/Startup.cs
+++ ASCOM.Alpaca.Simulators/Startup.cs
@@ -1,21 +1,13 @@
 using Blazored.Toast;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Reflection;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace ASCOM.Alpaca.Simulators
 {
@@ -48,19 +40,9 @@ namespace ASCOM.Alpaca.Simulators
 
             ASCOM.Alpaca.Razor.StartupHelpers.ConfigureSwagger(services, xmlPath);
             ASCOM.Alpaca.Razor.StartupHelpers.ConfigureAlpacaAPIBehavoir(services);
+            ASCOM.Alpaca.Razor.StartupHelpers.ConfigureAuthenticationService(services);
 
-            // configure basic authentication
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(
-                options =>
-                {
-                    options.SlidingExpiration = true;
-                    options.ExpireTimeSpan = TimeSpan.FromHours(1);
-                }
-                );
-
-            // configure DI for application services
             services.AddScoped<IUserService, UserService>();
-            services.AddScoped<AuthorizationFilter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,16 +62,10 @@ namespace ASCOM.Alpaca.Simulators
             }
 
             //Start Swagger on the Swagger endpoints if enabled.
-            
-            if (ServerSettings.RunSwagger)
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
-                                 $"{ServerSettings.ServerName} v1"));
-            }
+            Razor.StartupHelpers.ConfigureSwagger(app);
 
+            //Configure Discovery
             Razor.StartupHelpers.ConfigureDiscovery(app);
-
 
             //Serve static files, mostly CSS
             app.UseStaticFiles();
