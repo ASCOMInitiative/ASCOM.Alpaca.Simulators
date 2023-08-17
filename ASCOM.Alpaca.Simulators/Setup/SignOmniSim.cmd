7b474e7cfff685add50fe61dd6a224619d888b55
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-08-17 17:21:50 +0100

    Make Windows installer use the informational version text. Fix conflict with code signing certificate name resolution that was introduced by a Microsoft change.

--- ASCOM.Alpaca.Simulators/Setup/SignOmniSim.cmd
+++ ASCOM.Alpaca.Simulators/Setup/SignOmniSim.cmd
@@ -2,6 +2,6 @@ rem @echo off
 Rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 rem start "Sign with SHA256" /wait /b "signtool" sign /a /fd SHA256 /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 %1
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool" sign /a /fd SHA256 /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 %1
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool" sign /a /fd SHA256 /n "Peter Simpson" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 %1
 
 exit 0
