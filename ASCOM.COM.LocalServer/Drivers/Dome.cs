3c81c4e55b9eaa2aede84c56352dd021074564e1
Daniel Van Noord <sg1mash98@gmail.com>
2023-09-26 21:53:57 -0400

    Added Dome COM Driver

---
+++ ASCOM.COM.LocalServer/Drivers/Dome.cs
@@ -0,0 +1,92 @@
+﻿using ASCOM.DeviceInterface;
+using System.Runtime.InteropServices;
+
+namespace ASCOM.Simulators.LocalServer.Drivers
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Dome : BaseDriver, ASCOM.DeviceInterface.IDomeV2, IDisposable
+    {
+        public ASCOM.Common.DeviceInterfaces.IDomeV2 Device => (base.DeviceV2 as ASCOM.Common.DeviceInterfaces.IDomeV2);
+
+        public double Altitude => Device.Altitude;
+
+        public bool AtHome => Device.AtHome;
+
+        public bool AtPark => Device.AtPark;
+
+        public double Azimuth => Device.Azimuth;
+
+        public bool CanFindHome => Device.CanFindHome;
+
+        public bool CanPark => Device.CanPark;
+
+        public bool CanSetAltitude => Device.CanSetAltitude;
+
+        public bool CanSetAzimuth => Device.CanSetAzimuth;
+
+        public bool CanSetPark => Device.CanSetPark;
+
+        public bool CanSetShutter => Device.CanSetShutter;
+
+        public bool CanSlave => Device.CanSlave;
+
+        public bool CanSyncAzimuth => Device.CanSyncAzimuth;
+
+        public ShutterState ShutterStatus => (ShutterState)Device.ShutterStatus;
+
+        public bool Slaved { get => Device.Slaved; set => Device.Slaved = value; }
+
+        public bool Slewing => Device.Slewing;
+
+        public Dome()
+        {
+            base.GetDevice = () => ASCOM.Alpaca.DeviceManager.GetDome(0);
+        }
+
+        public void AbortSlew()
+        {
+            Device.AbortSlew();
+        }
+
+        public void CloseShutter()
+        {
+            Device.CloseShutter();
+        }
+
+        public void FindHome()
+        {
+            Device.FindHome();
+        }
+
+        public void OpenShutter()
+        {
+            Device.OpenShutter();
+        }
+
+        public void Park()
+        {
+            Device.Park();
+        }
+
+        public void SetPark()
+        {
+            Device.SetPark();
+        }
+
+        public void SlewToAltitude(double Altitude)
+        {
+            Device.SlewToAltitude(Altitude);
+        }
+
+        public void SlewToAzimuth(double Azimuth)
+        {
+            Device.SlewToAzimuth(Azimuth);
+        }
+
+        public void SyncToAzimuth(double Azimuth)
+        {
+            Device.SyncToAzimuth(Azimuth);
+        }
+    }
+}
