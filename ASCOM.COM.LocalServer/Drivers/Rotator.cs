83575f290cc13b2306d7e14871523bb000fabf7f
Daniel Van Noord <sg1mash98@gmail.com>
2023-09-27 18:13:25 -0400

    Added Rotator COM Driver

---
+++ ASCOM.COM.LocalServer/Drivers/Rotator.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ASCOM.Simulators.LocalServer.Drivers
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Rotator : BaseDriver, ASCOM.DeviceInterface.IRotatorV3, IDisposable
+    {
+        public ASCOM.Common.DeviceInterfaces.IRotatorV3 Device => (base.DeviceV2 as ASCOM.Common.DeviceInterfaces.IRotatorV3);
+
+        public bool CanReverse => Device.CanReverse;
+
+        public bool IsMoving => Device.IsMoving;
+
+        public float Position => Device.Position;
+
+        public bool Reverse { get => Device.Reverse; set => Device.Reverse = value; }
+
+        public float StepSize => Device.StepSize;
+
+        public float TargetPosition => Device.TargetPosition;
+
+        public float MechanicalPosition => Device.MechanicalPosition;
+
+        public Rotator()
+        {
+            base.GetDevice = () => ASCOM.Alpaca.DeviceManager.GetRotator(0);
+        }
+
+        public void Halt()
+        {
+            Device.Halt();
+        }
+
+        public void Move(float Position)
+        {
+            Device.Move(Position);
+        }
+
+        public void MoveAbsolute(float Position)
+        {
+            Device.MoveAbsolute(Position);
+        }
+
+        public void Sync(float Position)
+        {
+            Device.Sync(Position);
+        }
+
+        public void MoveMechanical(float Position)
+        {
+            Device.MoveMechanical(Position);
+        }
+    }
+}
