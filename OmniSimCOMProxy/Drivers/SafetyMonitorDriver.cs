aac5c6f6c537da6e0cefb3b5730bfbf5c2824d91
Daniel Van Noord <sg1mash98@gmail.com>
2024-05-08 21:22:33 -0400

    Added Safety Monitor COM Proxy

---
+++ OmniSimCOMProxy/Drivers/SafetyMonitorDriver.cs
@@ -0,0 +1,24 @@
+﻿using ASCOM.DeviceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCOM.Simulators.LocalServer.Drivers
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class SafetyMonitor : BaseDriver, ASCOM.DeviceInterface.ISafetyMonitorV3, IDisposable
+    {
+        ASCOM.Common.DeviceInterfaces.ISafetyMonitorV3 Device = new ASCOM.Com.DriverAccess.SafetyMonitor("OmniSim.SafetyMonitor");
+
+        public SafetyMonitor()
+        {
+            base.GetDevice = () => (ASCOM.Common.DeviceInterfaces.IAscomDeviceV2)Device;
+        }
+
+        public bool IsSafe => Device.IsSafe;
+    }
+}
